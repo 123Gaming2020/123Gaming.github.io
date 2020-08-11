b7168793c1afde8180f3a1ae5a45e13de8d0413d
123Gaming2020 <69522943+123Gaming2020@users.noreply.github.com>
2020-08-11 22:03:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = 'YOUR_CLIENT_ID';
+const CLIENT_ID = '360242505187-q5t84l1ja0lfp2f103v9pt7mcose7ui4.apps.googleusercontent.com';
 const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
 ];
