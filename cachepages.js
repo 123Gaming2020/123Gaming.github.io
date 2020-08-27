478d1ac0c216a7a106c4b8c5a07fdd2abd2077bd
123Gaming2020 <69522943+123Gaming2020@users.noreply.github.com>
2020-08-27 11:49:11 +0200

    Update cachepages.js

--- cachepages.js
+++ cachepages.js
@@ -7,13 +7,10 @@ const cacheAssets = [
 ];
 
 self.addEventListener('install', e => {
-  console.log('Service Worker: Installed');
-
   e.waitUntil(
     caches
       .open(cacheName)
       .then(cache => {
-        console.log('Service Worker: Caching Files');
         cache.addAll(cacheAssets);
       })
       .then(() => self.skipWaiting())
@@ -21,13 +18,11 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  console.log('Service Worker: Activated');
   e.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
           if (cache !== cacheName) {
-            console.log('Service Worker: Clearing Old Cache');
             return caches.delete(cache);
           }
         })
@@ -36,7 +31,10 @@ self.addEventListener('activate', e => {
   );
 });
 
-self.addEventListener('fetch', e => {
-  console.log('Service Worker: Fetching');
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
 });
